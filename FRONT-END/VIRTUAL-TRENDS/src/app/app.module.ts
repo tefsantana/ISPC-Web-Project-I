085824fc40de882d1d47f5e431d09c4d6e6f4a35
Carla Antonini <carla.antonini@hotmail.com>
2023-05-06 11:08:36 -0300

    integrando vistas producto y carpeta assets

--- FRONT-END/VIRTUAL-TRENDS/src/app/app.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 /*import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';*/
-import { HomeModule } from './home/home.module';
 import { ProductsModule } from './products/products.module';
-
+import { SistemaDeTallaModule } from './sistema-de-talla/sistema-de-talla.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [		
@@ -19,9 +19,11 @@ import { ProductsModule } from './products/products.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     HomeModule,
-    ProductsModule
+    ProductsModule,
+    SistemaDeTallaModule,
+    SharedModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/talla-estandar/talla-estandar.component.spec.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/talla-estandar/talla-estandar.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TallaEstandarComponent } from './talla-estandar.component';
 
@@ -9,14 +6,12 @@ describe('TallaEstandarComponent', () => {
   let component: TallaEstandarComponent;
   let fixture: ComponentFixture<TallaEstandarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ TallaEstandarComponent ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(TallaEstandarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
