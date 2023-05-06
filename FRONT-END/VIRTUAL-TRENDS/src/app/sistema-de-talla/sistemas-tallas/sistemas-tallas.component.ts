03322e57b2bdef7dc1a86bd48a969eeb3a0d0b82
RochettiUriel <urielrochetti@gmail.com>
2023-05-06 19:51:10 -0300

    Merge con rama de Andres y agregado de funcionalidad al boton de talle personalizado

--- FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/sistemas-tallas/sistemas-tallas.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/sistemas-tallas/sistemas-tallas.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPersonalizadoService } from 'src/app/services/sw-personalizado.service';
 
 @Component({
   selector: 'app-sistemas-tallas',
   templateUrl: './sistemas-tallas.component.html',
   styleUrls: ['./sistemas-tallas.component.css']
 })
-export class SistemasTallasComponent {
+export class SistemasTallasComponent implements OnInit{
+
+  tofPersonalizado:boolean = false;
+
+  constructor(private personalizadoSS: SwPersonalizadoService){
+
+  }
+
+  ngOnInit(){
+    
+    this.personalizadoSS.$tallePersonalizado.subscribe((valor)=> {this.tofPersonalizado = valor})
+
+  }
+
+  abrirPersonalizado(){
+
+    this.tofPersonalizado = true;
+
+  }
 
 }
