d880fbc433b632999d0f0bf14965f18a5d6adf57
Carla Antonini <carla.antonini@hotmail.com>
2023-05-07 08:18:25 -0300

    cambio home component ts

--- FRONT-END/VIRTUAL-TRENDS/src/app/home/home.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 /*import { HeaderComponent } from '/Users/gaghy/ProyectoIntegrador-E-COMMERCE/FRONT-END/VIRTUAL-TRENDS/src/app/shared/header/header.component';*/
 
 @Component({
@@ -6,13 +6,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent {}
 
-  ngOnInit(): void {
-  }
-
-}
 
 
