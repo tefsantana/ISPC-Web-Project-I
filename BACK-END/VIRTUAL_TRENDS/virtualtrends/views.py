1d92630b91fe0da700dfa453d2765c751532c95b
tefsantana <esantana@fi.uba.ar>
2023-06-02 02:17:15 -0300

    feature/api-rest-products: Se implementa URL de productos junto a su respectivo view y un json de mock dentro del View con sus metodos para corroborar que se vea al acceder a la ruta correspondiente.

--- BACK-END/VIRTUAL_TRENDS/virtualtrends/views.py
+++ BACK-END/VIRTUAL_TRENDS/virtualtrends/views.py
@@ -1,3 +1,29 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class ProductosView(View):
+    def get(self, request):
+        datos = {
+            'productos': [{
+                'name': 'producto 1',
+                'price': 1000
+            },
+            {
+                'name': 'producto 2',
+                'price': 2000
+            },
+            {
+                'name': 'producto 3',
+                'price': 3000
+            }]
+        }
+        return JsonResponse(datos)
+    def post(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
