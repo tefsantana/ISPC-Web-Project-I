9186adc8985c7bdf5838042de201cb4832536765
tefsantana <esantana@fi.uba.ar>
2023-06-01 22:16:56 -0300

    feature/error-page: Se implementa routing de Error Page 404

--- FRONT-END/VIRTUAL-TRENDS/src/app/pages/pages.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/pages/pages.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ErrorPageComponent],
   imports: [
     CommonModule
   ]
