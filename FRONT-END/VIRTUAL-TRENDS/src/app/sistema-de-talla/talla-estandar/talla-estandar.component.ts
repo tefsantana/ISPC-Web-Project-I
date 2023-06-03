6a875b615d9a8c18144c3e17630fee547fadb400
RochettiUriel <urielrochetti@gmail.com>
2023-06-03 01:26:16 -0300

    agrego servicio para enviar informacion de producto de un componente a otro

--- FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/talla-estandar/talla-estandar.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/sistema-de-talla/talla-estandar/talla-estandar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ɵcoerceToBoolean } from '@angular/core';
 import { SwEstandarService } from 'src/app/services/sw-estandar.service';
 import { RecibirTallasService } from 'src/app/services/tallas-services/recibir-tallas.service';
+import { ProductDataService } from 'src/app/services/data-services/product-data.service';
 
 @Component({
   selector: 'app-talla-estandar',
@@ -9,15 +10,18 @@ import { RecibirTallasService } from 'src/app/services/tallas-services/recibir-t
 })
 export class TallaEstandarComponent implements OnInit{
 
-  constructor (private estandarSS: SwEstandarService, private recibirTallas: RecibirTallasService){
+  constructor (private estandarSS: SwEstandarService, private recibirTallas: RecibirTallasService, private productData: ProductDataService){
 
   }
   
   seleccionado: string=""
   activos: string[] = this.recibirTallas.enviarTallasDisponibles()
-  
-  ngOnInit(): void {
-    
+  product_ID: any
+
+  ngOnInit() {
+    this.productData.recibirDatos().subscribe(dato => {
+      this.product_ID = dato;
+    });
   }
 
   esActivo(talle: string){
@@ -36,5 +40,12 @@ export class TallaEstandarComponent implements OnInit{
     this.estandarSS.$tallaEstandar.emit(false)
 
   }
+  
+  agregar_al_carrito(){
+    
+    console.log(this.product_ID)
+  }
 }
 
+
+this.https.get("https://localhost:8000/api/test")
