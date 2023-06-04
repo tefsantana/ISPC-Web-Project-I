f4a1f5f9870c039bdc006641c65a4160c86e11bf
Agostina <ago.95fiore@hotmail.com>
2023-06-04 12:45:55 -0300

    AF actualizo

--- FRONT-END/VIRTUAL-TRENDS/src/app/cuenta/cuenta.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/cuenta/cuenta.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginLogupComponent } from './login-logup/login-logup.component';
 import { RegistroComponent } from './registro/registro.component';
+import { EditarCuentaComponent } from './editar-cuenta/editar-cuenta.component';
 
 
 
 @NgModule({
   declarations: [
     LoginLogupComponent,
-    RegistroComponent
+    RegistroComponent,
+    EditarCuentaComponent
   ],
   imports: [
     CommonModule
