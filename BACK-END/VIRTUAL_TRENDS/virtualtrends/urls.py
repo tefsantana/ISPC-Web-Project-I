9c198342caaad8c6dc0ff20104382f19f5f1c7c5
Carla Antonini <carla.antonini@hotmail.com>
2023-06-04 23:19:28 -0300

    CA proporciono la path al newsletter

--- BACK-END/VIRTUAL_TRENDS/virtualtrends/urls.py
+++ BACK-END/VIRTUAL_TRENDS/virtualtrends/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TallaDeProductoView
+from .views import TallaDeProductoView 
+from .views import NewsletterView
 
 urlpatterns = [
-    path('talla-de-producto/', TallaDeProductoView.as_view(), name='talle-de-producto')
+    path('talla-de-producto/', TallaDeProductoView.as_view(), name='talle-de-producto'),
+    path('newsletter/', NewsletterView.as_view(), name='newsletter'),
 ]
