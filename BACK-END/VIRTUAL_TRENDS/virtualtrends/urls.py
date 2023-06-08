d7f7781bd097f2383b02882781bb6a48e91886fb
tefsantana <esantana@fi.uba.ar>
2023-06-08 02:13:11 -0300

    feature/api-rest-products: Se agrega ruta de productos y favoritos

--- BACK-END/VIRTUAL_TRENDS/virtualtrends/urls.py
+++ BACK-END/VIRTUAL_TRENDS/virtualtrends/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import TallaDeProductoView, LoginListView, LoginUpdateView, LoginQueryView, ProductListView, ImgProducView, RegistroView, ProductoAlCarritoView, NewsletterView, CrearTallaPersonalizada
+from .views import FavoritesView, TallaDeProductoView, LoginListView, LoginUpdateView, LoginQueryView, ProductListView, ImgProducView, RegistroView, ProductoAlCarritoView, NewsletterView, CrearTallaPersonalizada
 
 urlpatterns = [
     path('talla-de-producto/', TallaDeProductoView.as_view(), name='talle-de-producto'),
     path('login/', LoginListView.as_view(), name='login-list'),
     path('login/<str:email>/', LoginUpdateView.as_view(), name='login-update'),
     path('login/query/', LoginQueryView.as_view(), name='login-query'),
-    path('product/', ProductListView.as_view(), name='product-list'),
-    path('product/imgprod/<int:id_prod>/', ImgProducView.as_view(), name='img-prod'), 
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('product/imgprod/<int:id_prod>/', ImgProducView.as_view(), name='img-prod'),
+    path('favorites/', FavoritesView.as_view(), name='favorites'),
     path('logup/', RegistroView.as_view(), name='usuarios'), 
     path('producto-al-carrito/', ProductoAlCarritoView.as_view (), name='producto-al-carrito'), 
     path('newsletter/', NewsletterView.as_view(), name='newsletter'),
