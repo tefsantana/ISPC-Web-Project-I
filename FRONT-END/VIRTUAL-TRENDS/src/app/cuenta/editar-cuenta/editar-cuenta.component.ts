f0589bba7ea9e919c001570e6eaa5e21ccbb467b
Agostina <ago.95fiore@hotmail.com>
2023-06-11 20:11:58 -0300

    AF adjunto ts de editar cuenta

--- FRONT-END/VIRTUAL-TRENDS/src/app/cuenta/editar-cuenta/editar-cuenta.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/cuenta/editar-cuenta/editar-cuenta.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-editar-cuenta',
   templateUrl: './editar-cuenta.component.html',
   styleUrls: ['./editar-cuenta.component.css']
 })
 export class EditarCuentaComponent {
+  formulario: any;
+  constructor(private formBuilder: FormBuilder) {
+  }
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nombre: ['', Validators.required],
 
+      telefono: ['', Validators.pattern('[0-9]{10}')],
+    });
+  }
+  onSubmit() {
+
+  }
 }
