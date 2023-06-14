26b194699e500f9643dd777b7fd04ad75042efc0
Joaquin <joa17901@gmail.com>
2023-06-13 23:25:22 -0300

    GJ

--- FRONT-END/VIRTUAL-TRENDS/src/app/carrito/pantalla-del-carrito/pantalla-del-carrito.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/carrito/pantalla-del-carrito/pantalla-del-carrito.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarritoService } from '../../services/data-services/producto-al-carrito.service';
 
 @Component({
   selector: 'app-pantalla-del-carrito',
   templateUrl: './pantalla-del-carrito.component.html',
   styleUrls: ['./pantalla-del-carrito.component.css']
 })
-export class PantallaDelCarritoComponent {
+export class PantallaDelCarritoComponent implements OnInit {
+  productos: any[] = [];
 
+  constructor(private carritoService: CarritoService) { }
+
+  ngOnInit(): void {
+    const dni = '123456789'; 
+
+    this.carritoService.getCarrito(dni)
+      .then(productos => this.productos = productos)
+      .catch(error => console.error(error));
+  }
 }
