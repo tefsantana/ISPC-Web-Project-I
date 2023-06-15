bd3c8044c4ab67db9624f8d153691a28f6e8155b
tefsantana <esantana@fi.uba.ar>
2023-06-15 04:16:38 -0300

    feature/admin: Componente principal que redirige a la administracion de productos o de usuarios para el panel de control de Admin

--- FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from '../services/navigation/navigation.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private navigation: NavigationService) { }
 
   ngOnInit() {
+  }
+
+  navigateToProductsAdmin() {
+    this.navigation.navigateToProductsAdmin();
+  }
+
+  navigateToUsersAdmin() {
+    this.navigation.navigateToUsersAdmin();
   }
 
 }
