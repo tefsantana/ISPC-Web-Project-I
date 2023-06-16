f0bb21007ee0f4466dde07ca5fb9f69494927ed8
Joaquin <joa17901@gmail.com>
2023-06-16 20:39:19 -0300

    GJ- merge con dev-Sprint4

--- FRONT-END/VIRTUAL-TRENDS/src/app/carrito/pantalla-del-carrito/pantalla-del-carrito.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/carrito/pantalla-del-carrito/pantalla-del-carrito.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoAlCarritoService } from '../../services/data-services/producto-al-carrito.service';
 import { NavigationService } from 'src/app/services/navigation/navigation.service';
+type productosCarrito={ 
+  id_prod: number,
+  id_car: number,
+  cantidad:number,
+  talla: number,
+  color: string,
+  espersonalizado: boolean
+}
 
 @Component({
   selector: 'app-pantalla-del-carrito',
@@ -8,9 +16,9 @@ import { NavigationService } from 'src/app/services/navigation/navigation.servic
   styleUrls: ['./pantalla-del-carrito.component.css']
 })
 export class PantallaDelCarritoComponent implements OnInit {
-  productos: any[] = [];
+  productos: productosCarrito[]=[];
 
-  constructor(private carritoService: ProductoAlCarritoService, private navigation: NavigationService) { }
+  constructor(private productosCarrito: ProductoAlCarritoService, private navigation: NavigationService) { }
 
   ngOnInit(): void {}
 
--- FRONT-END/VIRTUAL-TRENDS/src/app/services/data-services/producto-al-carrito.service.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/services/data-services/producto-al-carrito.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DniDataService } from './dni-data.service';
+type productosCarrito={ 
+  id_prod: number,
+  id_car: number,
+  cantidad:number,
+  talla: number,
+  color: string,
+  espersonalizado: boolean
+}
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductoAlCarritoService {
+
+  contenidoCarrito: productosCarrito []=[];
 
   constructor(private http:HttpClient, private dniData:DniDataService){}
 
@@ -16,8 +26,12 @@ export class ProductoAlCarritoService {
 
     this.dniData.recibirDNI().subscribe(dniRecibido => {
 
-      return this.http.get('https://localhost:8000/api/consultar-carrito/?dni=${dniRecibido}');
-
+      this.http.get<productosCarrito[]>('https://localhost:8000/api/consultar-carrito/?dni=${dniRecibido}').subscribe(carrito=>{
+        this.contenidoCarrito=carrito
+      })
     })
+
+    return this.contenidoCarrito
+
   }
 }
