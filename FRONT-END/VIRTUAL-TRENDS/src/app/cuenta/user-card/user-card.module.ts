11edc9009a1fa6baa44d1ebc9db3e48fe5a10f85
Agostina <ago.95fiore@hotmail.com>
2023-06-16 20:33:16 -0300

    AF creo modulo user-card

---
+++ FRONT-END/VIRTUAL-TRENDS/src/app/cuenta/user-card/user-card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserCardComponent } from './user-card.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UserCardComponent]
+})
+export class UserCardModule { }
