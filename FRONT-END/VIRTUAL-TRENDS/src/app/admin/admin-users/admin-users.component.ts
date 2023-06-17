48a87e6554f61c0ddc643295e2b471301b677e4c
tefsantana <esantana@fi.uba.ar>
2023-06-17 00:53:26 -0300

    feature/admin: Se llama al servicio que trae todos los usuarios y a su vez se formatea el DNI

--- FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin-users/admin-users.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin-users/admin-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { GetUsersService } from 'src/app/services/admin/getUsers.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -9,10 +10,19 @@ export class AdminUsersComponent implements OnInit {
   public formType: string = '';
   // usuario: usuario.usuario[] = [];
   @Input() type: string = '';
+  users = [];
 
-  constructor() { }
+  constructor(private UsersService: GetUsersService) { }
 
   ngOnInit() {
+    this.UsersService.get().subscribe((data: any) => {
+      this.users = data;
+      Array.from(this.users).forEach((user: any) => {
+        user.dni = (user.dni).toLocaleString();
+        user.dni = user.dni.split(',').join('.');
+      });
+    }
+    );
   }
   showForm(id: string) {
     this.formType = 'delete';
