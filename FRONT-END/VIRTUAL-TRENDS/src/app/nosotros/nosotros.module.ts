ec3beed8cd26d8923329fbb939b25976ba2d75eb
Carla Antonini <carla.antonini@hotmail.com>
2023-06-17 03:27:12 -0300

    CA modificacion de module nosotros

--- FRONT-END/VIRTUAL-TRENDS/src/app/nosotros/nosotros.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/nosotros/nosotros.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarrouselComponent
+  ],
   imports: [
     CommonModule
   ]
