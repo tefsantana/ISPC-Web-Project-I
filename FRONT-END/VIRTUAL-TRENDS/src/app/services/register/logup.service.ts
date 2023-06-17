bf8a0e3f3defefc679070e9591a0c88523ae7ec1
MDLDeveloper <mariodanielledesma91@gmail.com>
2023-06-17 16:07:22 -0300

    MDL - Se soluciona bug del servicio de registro, se mejoran validaciones y manejo de errores

--- FRONT-END/VIRTUAL-TRENDS/src/app/services/register/logup.service.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/services/register/logup.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogupService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  logup(register:any):Observable<any>{
+    return this.http.post('http://127.0.0.1:8000/api/logup/', register);
+  }
 }
