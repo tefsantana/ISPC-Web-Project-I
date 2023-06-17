f41147a0dad2c4a331acc55c546483a8a5a32a64
Carla Antonini <carla.antonini@hotmail.com>
2023-06-17 04:02:04 -0300

    CA agrego el componente NosotrosDashboard a las declaraciones y exports

--- FRONT-END/VIRTUAL-TRENDS/src/app/nosotros/nosotros.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/nosotros/nosotros.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NosotrosDashboardComponent } from './nosotros-dashboard/nosotros-dashboard.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
+
 
 
 
 @NgModule({
   declarations: [
-    CarrouselComponent
+    NosotrosDashboardComponent,
+    CarrouselComponent,
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    NosotrosDashboardComponent,
+    CarrouselComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
 })
 export class NosotrosModule { }
