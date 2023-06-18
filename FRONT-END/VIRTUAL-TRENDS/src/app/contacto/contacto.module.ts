4526da64fec7f74670f656d07eea765746c7c359
Carla Antonini <carla.antonini@hotmail.com>
2023-06-18 19:44:41 -0300

    CA Service para modulo  Contacto

--- FRONT-END/VIRTUAL-TRENDS/src/app/contacto/contacto.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/contacto/contacto.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardContactoComponent } from './dashboard-contacto/dashboard-contacto.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { DashboardContactoComponent } from './dashboard-contacto/dashboard-conta
     DashboardContactoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ]
 })
 export class ContactoModule { }
--- FRONT-END/VIRTUAL-TRENDS/src/app/contacto/dashboard-contacto/dashboard-contacto.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/contacto/dashboard-contacto/dashboard-contacto.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DashboardContactoMocksService } from '../../services/mocks/dashboard-contacto-mocks.service';
 
 @Component({
   selector: 'app-dashboard-contacto',
   templateUrl: './dashboard-contacto.component.html',
   styleUrls: ['./dashboard-contacto.component.css']
 })
-export class DashboardContactoComponent {
 
+
+export class DashboardContactoComponent {
+  formularioContacto: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private contactoService: DashboardContactoMocksService
+  ) {
+    this.formularioContacto = this.fb.group({
+      nombreCompleto: '',
+      telefono: '',
+      email: '',
+      mensaje: ''
+    });
+  }
+
+  enviarFormularioContacto() {
+    const formulario = this.formularioContacto.value;
+    console.log('Datos del formulario:', formulario);
+    this.contactoService.enviarFormularioContacto(formulario);
+  }
+
+  obtenerContactos() {
+    const contactos = this.contactoService.obtenerContactos();
+    console.log(contactos);
+  }
 }
+
