4898a2a181a1b6eacf7a0e2561ad70f2b065d6dd
Joaquin <joa17901@gmail.com>
2023-06-20 22:51:46 -0300

    GJ- Cambios en la formula del carrito

--- FRONT-END/VIRTUAL-TRENDS/src/app/services/data-services/product-carrito.service.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/services/data-services/product-carrito.service.ts
@@ -8,12 +8,24 @@ import { Observable } from 'rxjs';
 export class ProductCarritoService {
 
   constructor(private http: HttpClient, private productData: ProductDataService) { }
+
+  datos_del_producto: any;
+
+
+  datosProducto(){
+
+    this.productData.productoCarrito().subscribe(Producto => {
+
+      this.datos_del_producto = Producto
+
+    })
+
+  }
   
 
   agregarCarrito(){
-    this.productData.productoCarrito().subscribe(Producto => {
-      console.log(Producto)
-      this.http.post('http://localhost:8000/api/producto-al-carrito/', Producto).subscribe(
+      console.log(this.datos_del_producto)
+      this.http.post('http://localhost:8000/api/producto-al-carrito/', this.datos_del_producto).subscribe(
         response => {
           console.log("La solicitud POST se realizó con éxito.");
           console.log(response);
@@ -22,7 +34,6 @@ export class ProductCarritoService {
           console.error("Error al realizar la solicitud POST:", error);
         }
       );
-    } )
   }
 
   /*agregarCarrito(producto: any): Observable<any>{
