6806472c11cd46465ef6fd6dcd1fe753fb9cc551
tefsantana <esantana@fi.uba.ar>
2023-06-20 21:28:30 -0300

    feature/admin: Ajustes de validaciones de Admin de productos

--- FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin-products/admin-products-form/admin-products-form.component.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/admin/admin-products/admin-products-form/admin-products-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GetCategoriesService } from 'src/app/services/admin/getCategories.service';
-import { GetProductService } from 'src/app/services/admin/getProduct.service';
+import { ProductsService } from 'src/app/services/admin/adminProducts.service';
 import { Products } from 'src/app/utils/products';
 import { AdminProductsComponent } from '../admin-products.component';
 import { BehaviorSubject } from 'rxjs';
@@ -15,22 +15,20 @@ export class AdminProductsFormComponent implements OnInit {
 
   @Input() type: string = '';
   @Input() showModifyForm: boolean = false;
-  currentUserData: BehaviorSubject<UserData> = new BehaviorSubject<UserData>({dni:11111111});
   colorsList: Products.Colors[] = ["dark-red", "dark-green", "purple-grey", "orange", "coral", "cyan"];
   sizesList: Products.Sizes[] = ["XS", "S", "M", "L", "XL"];
   id_prod: string = '';
+  categorias: string[] = [];
   newProduct: Products.AdminProduct = {
-    dni: this.currentUserData.value.dni,
     nombre: "",
     descripcion: "",
     precio: 0,
     colores: [],
     tallas: [],
     imagenes: [],
-    categoria: ""
+    categoria: "Remera"
   };
   modifiedProduct: Products.AdminProduct = {
-    dni: this.currentUserData.value.dni,
     id_prod: 0,
     nombre: "",
     descripcion: "",
@@ -38,11 +36,11 @@ export class AdminProductsFormComponent implements OnInit {
     colores: [],
     tallas: [],
     imagenes: [],
-    categoria: ""
+    categoria: "Remera",
+    eliminar: false
   };
   idProductoValidationModify: boolean = false;
   idProductoValidationDelete: boolean = false;
-  categorias: string[] = [];
   nameValidation: boolean = false;
   descriptionValidation: boolean = false;
   priceValidation: boolean = false;
@@ -55,11 +53,16 @@ export class AdminProductsFormComponent implements OnInit {
   };
   categoryValidation: boolean = true;
   showErrorText: boolean = false;
+  showNotFoundText: boolean = false;
+  showProgressTextAdd: boolean = false;
+  showProgressTextModify: boolean = false;
+  showProgressTextDelete: boolean = false;
   showSuccessTextModify: boolean = false;
   showSuccessTextDelete: boolean = false;
   showSuccessTextAdd: boolean = false;
+  showDeletedProductAdvertisement: boolean = false;
 
-  constructor(private AdminProduct: GetProductService, private AdminCategories: GetCategoriesService, private AdminParentComponent: AdminProductsComponent) { }
+  constructor(private AdminProducts: ProductsService, private AdminCategories: GetCategoriesService, private AdminParentComponent: AdminProductsComponent) { }
 
   ngOnInit() {
     this.AdminCategories.get().subscribe((data: any) => {
@@ -111,7 +114,7 @@ export class AdminProductsFormComponent implements OnInit {
   resetState() {
     // Esta función es para resetear el estado inicial de los inputs para no enviar datos erróneos ya que los datos que se reciben del servicio son solo para mostrar.
     this.modifiedProduct = {
-      dni: this.currentUserData.value.dni,
+      id_prod: 0,
       nombre: "",
       descripcion: "",
       precio: 0,
@@ -123,20 +126,32 @@ export class AdminProductsFormComponent implements OnInit {
   }
 
   modifyProduct() {
+    this.showModifyForm = false;
+    this.resetState();
     this.id_prod = (document.getElementById('id-producto-modify') as HTMLInputElement).value;
-    this.AdminProduct.get(this.id_prod).subscribe((data: any) => {
+    this.AdminProducts.get(this.id_prod).subscribe((data: any) => {
+      this.showNotFoundText = false;
+      this.showModifyForm = true;
       this.modifiedProduct = data;
-      Array.from(this.categorias).forEach((categoria) => {
-        if (categoria === this.modifiedProduct.categoria) {
-          (document.getElementById('categories-select') as HTMLSelectElement).value = categoria;
-        }
-      });
     });
-    this.showModifyForm = !this.showModifyForm;
+    setTimeout(() => {
+      (document.getElementById('categories-select') as HTMLSelectElement).value = this.modifiedProduct.categoria;
+    }, 100);
+    this.showNotFoundText = this.modifiedProduct.id_prod === 0 ? true : false;
   }
 
   deleteProduct() {
-    this.showSuccessTextDelete = true;
+    this.id_prod = (document.getElementById('id-producto-delete') as HTMLInputElement).value;
+    this.AdminProducts.delete(Number(this.id_prod)).subscribe((data: any) => {
+      if (data.status === 200) {
+        this.showNotFoundText = false;
+        this.showSuccessTextDelete = true;
+      }
+      else {
+        this.showNotFoundText = true;
+      }
+    });
+
   }
 
   addProduct(e: any) {
@@ -174,7 +189,14 @@ export class AdminProductsFormComponent implements OnInit {
       this.newProduct.imagenes.push((document.getElementById('second-image-input') as HTMLInputElement).value);
       this.newProduct.imagenes.push((document.getElementById('third-image-input') as HTMLInputElement).value);
 
-      this.showSuccessTextModify = this.colorsValidation && this.sizesValidation;
+      this.showProgressTextAdd = this.colorsValidation && this.sizesValidation;
+
+      if (this.showProgressTextAdd) {
+        this.AdminProducts.add(this.newProduct).subscribe((data: any) => {
+          this.showSuccessTextAdd = true;
+        });
+      }
+
     }
     else if (this.type === 'modify') {
       this.resetState();
@@ -206,7 +228,7 @@ export class AdminProductsFormComponent implements OnInit {
 
       this.showErrorText = !this.colorsValidation || !this.sizesValidation;
 
-      this.modifiedProduct.id_prod = Number((document.getElementById('id-producto') as HTMLInputElement).value);
+      this.modifiedProduct.id_prod = Number((document.getElementById('id-producto-modify') as HTMLInputElement).value);
       this.modifiedProduct.nombre = (document.getElementById('name-input') as HTMLInputElement).value;
       this.modifiedProduct.descripcion = (document.getElementById('description-input') as HTMLInputElement).value;
       this.modifiedProduct.precio = Number((document.getElementById('price-input') as HTMLInputElement).value);
@@ -221,7 +243,13 @@ export class AdminProductsFormComponent implements OnInit {
         this.modifiedProduct.imagenes.push((document.getElementById('third-image-input') as HTMLInputElement).value);
       }
 
-      this.showSuccessTextModify = this.colorsValidation && this.sizesValidation;
+      this.showProgressTextModify = this.colorsValidation && this.sizesValidation;
+
+      if (this.showProgressTextModify) {
+        this.AdminProducts.update(this.modifiedProduct).subscribe((data: any) => {
+          this.showSuccessTextModify = data ? true : false;
+        });
+      }
     }
   }
 
